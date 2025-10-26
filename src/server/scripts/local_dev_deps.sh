0d8a6a69e36de3ea5eb6c51fbe822d8afef5b642
AutoLTX <tingxilin@gmail.com>
2025-10-26 18:29:55 +0800

    chore: update scripts to use seaweedfs

--- src/server/scripts/local_dev_deps.sh
+++ src/server/scripts/local_dev_deps.sh
@@ -7,7 +7,7 @@ DATABASE_LOCATION="./db/dev_data" \
 REDIS_LOCATION="./db/dev_redis" \
 RABBITMQ_LOCATION="./db/dev_rabbitmq" \
 S3_LOCATION="./db/dev_s3" \
-docker compose up acontext-server-pg acontext-server-redis acontext-server-rabbitmq acontext-server-minio-setup acontext-server-minio
+docker compose up acontext-server-pg acontext-server-redis acontext-server-rabbitmq acontext-server-seaweedfs-setup acontext-server-seaweedfs
 rm -rf ./db/dev_data
 rm -rf ./db/dev_redis
 rm -rf ./db/dev_rabbitmq
